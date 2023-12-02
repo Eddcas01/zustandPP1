29a63f9cb52411c4b7f83fce68ad3e424a169539
Eddcas01 <eddcas01@gmail.com>
2023-12-01 21:09:03 -0600

    Culminación seccion 3 del curso - middlewares session storage y firebase

--- src/stores/bears/bears.storage.ts
+++ src/stores/bears/bears.storage.ts
@@ -15,60 +15,60 @@ interface BearState {
   polarBears: number;
   pandaBears: number;
 
-  bears: Bear [];
+  bears: Bear[];
 
-  computed:{
-totalBears: number;
-  },
+  //pasa a ser una funcion porque en el middleware persist no permite funcionar al getter de forma correcta
+    totalBears: () => number;
+  
 
   increaseBlackBears: (by: number) => void;
   increasePolarBears: (by: number) => void;
   increasePandaBears: (by: number) => void;
 
 
-  doNothing:() =>  void;
+  doNothing: () => void;
   addBears: () => void;
-  clearBears:() => void;
+  clearBears: () => void;
 }
 
-const storeAPIBears : StateCreator<BearState> = (set, get) => ({
+const storeAPIBears: StateCreator<BearState> = (set, get) => ({
   blackBears: 10,
   polarBears: 10,
   pandaBears: 1,
 
-bears: [{id:1, name:'oso 1'}],
+  bears: [{ id: 1, name: 'oso 1' }],
 
-computed:{
+ 
+//se cambia a una  funcion que devuelve un numero
+    totalBears: () =>  {
+    
+      return get().blackBears + get().polarBears + get().pandaBears + get().bears.length;
 
-get  totalBears(): number{
+    },
 
-  return get().blackBears + get().polarBears + get().pandaBears + get().bears.length;
-
-}
-
-},
+  
   increaseBlackBears: (by: number) => set((state) => ({ blackBears: state.blackBears + by })),
   increasePolarBears: (by: number) => set((state) => ({ polarBears: state.polarBears + by })),
   increasePandaBears: (by: number) => set((state) => ({ pandaBears: state.pandaBears + by })),
 
-  doNothing:() => set(state =>({bears:[...state.bears]})),
-  addBears:() => set(state =>({
-    bears:[ ...state.bears,{id: state.bears.length +1, name:`oso #${state.bears.length +1}`}]
-  
-  
+  doNothing: () => set(state => ({ bears: [...state.bears] })),
+  addBears: () => set(state => ({
+    bears: [...state.bears, { id: state.bears.length + 1, name: `oso #${state.bears.length + 1}` }]
+
+
   })),
-  clearBears:() => set(({bears:[]}))
+  clearBears: () => set(({ bears: [] }))
 })
 
 export const useBearStorage = create<BearState>()(
 
-persist(
-storeAPIBears,
+  persist(
+    storeAPIBears,
 
-{name: 'storageBears'}
+    { name: 'storageBears' }
 
 
-)
+  )
 
 )
 
